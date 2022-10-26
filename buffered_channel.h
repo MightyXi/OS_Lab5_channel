eb07966790ca8e460d76edabc236e7564e82d079
MightyXi <100283671+MightyXi@users.noreply.github.com>
2022-10-26 23:25:10 +0300

    Update buffered_channel.h

--- buffered_channel.h
+++ buffered_channel.h
@@ -1,51 +1,66 @@
-#include <thread>
+#ifndef BUFFERED_CHANNEL_H_
+#define BUFFERED_CHANNEL_H_
+
+#include <queue>
 #include <mutex>
 #include <condition_variable>
-#include <queue>
 
-std::mutex sLocker;
-std::mutex rLocker;
-std::condition_variable newsignal;
+std::mutex write_mutex;
+std::mutex read_mutex;
+std::condition_variable write_variable;
+std::condition_variable read_variable;
+bool can_write = true;
+bool can_read = true;
+bool closed = false;
 
-template <class T>
-struct Channel{
+
+template<class T>
+class Channel {
 private:
-std::queue<T> buff;
-int buffer_size;
-bool closed;
+    const int buffer_size;
+    std::queue<T> buff;
+
 public:
-    Channel(int size){
-      buffer_size = size;
+    explicit Channel(int size) : buffer_size(size) {
+        can_write = buff.size() < buffer_size;
+        can_read = !buff.empty();
     }
 
-    void send(T value){
-        if (closed) {
-            throw std::runtime_error("channel is closed");
-        }
-        std::unique_lock<std::mutex> locker(sLocker);
-        while (buff.size() >= buffer_size) {
-            newsignal.wait(locker);
-        }
+    void Send(T value) {
+        if(closed)
+            throw std::runtime_error("The channel is closed.");
+        std::unique_lock<std::mutex> lock(write_mutex);
+        while(!can_write)
+            write_variable.wait(lock);
         buff.push(value);
-        newsignal.notify_one();
+        can_write = buff.size() < buffer_size;
+        can_read = !buff.empty();
+        write_variable.notify_one();
+        read_variable.notify_one();
     }
 
     std::pair<T, bool> Recv() {
-        if (closed && buff.size() >= buffer_size) {
+        if(closed && buff.empty())
             return {T(), false};
-        }
-        std::unique_lock<std::mutex> locker(rLocker);
-        while (buff.empty()) {
-            newsignal.wait(locker);
-        }
+        std::unique_lock<std::mutex> lock(read_mutex);
+        while(!can_read)
+            read_variable.wait(lock);
+        if(buff.empty())
+            return {T(), false};
         T value = buff.front();
-            buff.pop();
-        newsignal.notify_one();
-        return {value, false};
+        buff.pop();
+        can_write = buff.size() < buffer_size;
+        can_read = !buff.empty();
+        write_variable.notify_one();
+        read_variable.notify_one();
+        return {value, true};
     }
 
     void Close() {
         closed = true;
+        read_variable.notify_one();
+        write_variable.notify_one();
     }
-
 };
+
+#endif // BUFFERED_CHANNEL_H_
